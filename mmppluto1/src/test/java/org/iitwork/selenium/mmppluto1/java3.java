e06651fb2387a530b58a4510bf974c95474805b4
Shobha8 <shobhaprakash2013@gmail.com>
2023-10-27 14:27:19 -0400

    added java 3

---
+++ mmppluto1/src/test/java/org/iitwork/selenium/mmppluto1/java3.java
@@ -0,0 +1,9 @@
+package org.iitwork.selenium.mmppluto1;
+
+public class java3 {
+
+	public static void main(String[] args) {
+		System.out.println("Automation takes long time to practice");
+	}
+
+}
