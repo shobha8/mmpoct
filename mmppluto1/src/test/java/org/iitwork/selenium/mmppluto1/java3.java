63f6d79a62b966d6eb3c01e7bba95fd513abf3f2
shobha8 <128195487+shobha8@users.noreply.github.com>
2023-10-27 14:58:02 -0400

    Update java3.java

--- mmppluto1/src/test/java/org/iitwork/selenium/mmppluto1/java3.java
+++ mmppluto1/src/test/java/org/iitwork/selenium/mmppluto1/java3.java
@@ -3,7 +3,8 @@ package org.iitwork.selenium.mmppluto1;
 public class java3 {
 
 	public static void main(String[] args) {
-		System.out.println("Automation takes long time to practice");
+		System.out.println("coding is tough");
+		System.out.println("coding is tough");
 	}
 
 }
